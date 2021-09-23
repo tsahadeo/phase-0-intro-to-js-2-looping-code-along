2b6110811b1843568530ee0970b8442b22fea19d
Travis <tsahadeo@mac.com>
2021-09-23 10:04:41 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const thankYouCards = ["Ada", "Brendan", "Ali"];
+
+function writeCards(thankYouCards){
+    let message = []
+for (let i = 0; i < thankYouCards.length; i++){
+    message.push( `Thank you, ${thankYouCards[i]}, for the wonderful surprise gift!`)
+    }
+    return message
+}
+
+function countDown(){
+    let countDown = 10
+    while (countDown > -1) {
+        console.log(countDown--)
+    } 
+}
